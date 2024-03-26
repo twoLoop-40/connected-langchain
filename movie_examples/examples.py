d5208991a1bdfc23c6a67567a8de5a6d012cd146
joonho <jh.lee@spira-t.com>
2024-03-27 04:11:34 +0900

    movie_info

---
+++ movie_examples/examples.py
@@ -0,0 +1,25 @@
+examples = [
+  {
+    "movie_name": "미지수",
+    "answer": """
+      "director": "이돈구",
+      "production_year": 2023,
+      "production_country": "한국",
+      "type": "장편",
+      "genres": ["드라마", "멜로/로맨스", "가족", "판타지", "SF"],
+      "production_status": "개봉예정",
+    """
+
+  },
+  {
+    "movie_name": "동정뺏은 새엄마",
+    "answer": """
+      "director": "사쿠라비토",
+      "production_year": 2023,
+      "production_country": "일본",
+      "type": "장편",
+      "genres": ["멜로/로맨스"],
+      "production_status": "기타",
+    """
+  }
+]
